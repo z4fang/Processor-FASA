6a72523ba12040cfb61b2c1ff7945a89e1b7b7ce
ZhongKang Fang <z4fang@ucsd.edu>
2022-03-10 16:47:22 -0800

    New ISA

--- Definitions.sv
+++ Definitions.sv
@@ -42,19 +42,31 @@ package Definitions;
     const logic [2:0]kSUB  = 3'b101;
     const logic [2:0]kCLR  = 3'b110;
     */
-    const logic [2:0]kADD  = 3'b000;
-    const logic [2:0]kXOR  = 3'b001;
-    const logic [2:0]kOR  = 3'b010;
-    const logic [2:0]kLOD  = 3'b011;
-    const logic [2:0]kSTR  = 3'b100;
-    const logic [2:0]kBGZ  = 3'b101;
-    const logic [2:0]kSLL  = 3'b110;
-    const logic [2:0]kAND  = 3'b111;
+    const logic [3:0]kADD  = 4'b0000;
+    const logic [3:0]kXOR  = 4'b0001;
+    const logic [3:0]kORR  = 4'b0010;
+    const logic [3:0]kLOD  = 4'b0011;
+    const logic [3:0]kSTR  = 4'b0100;
+    const logic [3:0]kBNE  = 4'b0101;
+    const logic [3:0]kSLL  = 4'b0110;
+    const logic [3:0]kSRL  = 4'b0111;
+    const logic [3:0]kAND  = 4'b1000;
+    const logic [3:0]kXXR  = 4'b1001;
+    const logic [3:0]kCPP  = 4'b1010;
+    const logic [3:0]kCYY  = 4'b1011;
+    const logic [3:0]kSUB  = 4'b1100;
+    const logic [3:0]kBEQ  = 4'b1101;
+    const logic kRTYPE     = 1'b0;
+    const logic kITYPE     = 1'b1;
+    //const logic [3:0]kLDI  = 1'b1;
+    //const logic [3:0]kADD  = 4'b0000;
+    //const logic [3:0]kADD  = 4'b0000;
+    
 
    // Modern SystemVerilog lets you define types. The advantage
    // of doing this is that tools can preserve type metadata,
    // e.g. enum names will appear in timing diagrams
    typedef enum logic [2:0] {
-       ADD, XOR, OR, LOD,
-        STR, BGZ, SLL, AND } op_mne;
+       ADD, XOR, ORR, LOD, STR, BNE, SLL, SRL,
+       AND, XXR, CPP, CYY, SUB, BEQ } op_mne;
 endpackage // Definitions
